d8389a3b8abf1859e3ce63daaa6c80dfb06141ab
TheMaximeSan <mxm.tournier@gmail.com>
2026-06-10 19:42:19 +0200

    update

--- app/head.php
+++ app/head.php
@@ -32,7 +32,7 @@ function head($page)
     echo '<!doctype html> <html lang="fr"> <head>';
     require_once "composant/meta.phtml";
     echo '<title>' . $title . '</title>';
-    echo '<link href="' . $env_urlLogoOnglet . '" rel="shortcut icon">';
+    echo '<link href="' . $env_urlLogoOnglet . '" rel="shortcut icon" type="image/png">';
     require_once "extension.php";
     echo '</head>';
 }
